e8e8c1d0fbf4b8282e2f22c5f492bd05a91eb05e
Pann <himari.fb@gmail.com>
2025-10-22 01:27:35 +0700

    fix github đăng nhập lần 1

--- obj/Debug/net9.0/demo_project.AssemblyInfo.cs
+++ obj/Debug/net9.0/demo_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c33fc418f75f706d2901d1eac5810884538baf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4a9c5b19bb3854b1eec1a0e384e46bc1a47acf")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
